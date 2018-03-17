0a388d4e8c3eab5b9edb0b3d0192060eb3d8d60a
RIM\Juan.Pablo.Perez <juan.pablo@retailinmotion.com>
2018-03-17 12:27:52 +0000

    Creating a Dynamic Menu

--- src/main.js
+++ src/main.js
@@ -3,8 +3,18 @@ import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-
-Vue.use(Vuetify)
+import colors from 'vuetify/es5/util/colors'
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.red.darken2,
+    accent: colors.red.accent2,
+    secondary: colors.grey.lighten1,
+    info: colors.blue.lighten1,
+    warning: colors.amber.darken2,
+    error: colors.red.accent4,
+    success: colors.green.lighten2
+  }
+})
 
 Vue.config.productionTip = false
 

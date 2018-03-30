10362093eed29861e6a6b12f011aa46fe6d4c65a
RIM\Juan.Pablo.Perez <juan.pablo@retailinmotion.com>
2018-03-30 10:01:56 +0100

    Fetching User State

--- src/store/index.js
+++ src/store/index.js
@@ -225,6 +225,31 @@ export const store = new Vuex.Store({
         fbKeys: {}
       })
     },
+    fetchUserData  ({ commit, getters }) {
+      commit('setLoading', true)
+      firebase.database().ref('/users/' + getters.user.id + '/registrations/')
+        .once('value')
+        .then(data => {
+          const dataPairs = data.val()
+          let registeredMeetups = []
+          let swappedPairs = {}
+          for (let key in dataPairs) {
+            registeredMeetups.push(dataPairs[key])
+            swappedPairs[dataPairs[key]] = key
+          }
+          const updatedUser = {
+            id: getters.user.id,
+            registeredMeetups: registeredMeetups,
+            fbKeys: swappedPairs
+          }
+          commit('setLoading', false)
+          commit('setUser', updatedUser)
+        })
+        .catch(error => {
+          commit('setLoading', false)
+          console.log(error)
+        })
+    },
     logout ({ commit }) {
       firebase.auth().signOut()
       commit('setUser', null)
